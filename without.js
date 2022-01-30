8397542860c8a59958cbd21aa9b856b956037f3a
Harshbir Singh <singh.hveer1@gmail.com>
2022-01-30 10:16:47 -0700

    Implemented without function

---
+++ without.js
@@ -0,0 +1,47 @@
+//Function to compare two arrays
+const eqArrays = function(array1, array2) {
+  let returnValue = false;
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] === array2[i]) {
+        returnValue = true;
+      } else {
+        returnValue = false;
+      }
+    }
+  }
+  return returnValue;
+};
+
+// Function to print appropriate message
+const assertArraysEqual = function(array1, array2) {
+  let comparisonResult = eqArrays(array1, array2);
+  if (comparisonResult === true) {
+    console.log("✅ Both Array's are Equal");
+  } else {
+    console.log("❌ Both Array's are not Equal");
+  }
+   
+};
+
+const without = function(source, itemsToremove) {
+  let newArray = source.slice();
+  for (let i = 0; i < itemsToremove.length; i++) {
+    for (let j = 0; j < newArray.length; j++) {
+
+      if (itemsToremove[i] === newArray[j]) {
+        newArray.splice(j,1);
+      }
+    }
+  }
+  return newArray;
+};
+
+//TEST CODE
+console.log(without([1, 2, 3], [1])); // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"])
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]);
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
