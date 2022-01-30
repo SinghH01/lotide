2c4ddb3802ba260842f5b8e62848d619b77bae92
Harshbir Singh <singh.hveer1@gmail.com>
2022-01-30 08:22:47 -0700

    Created asertArrayEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,32 @@
+//Function to compare two arrays
+const eqArrays = function(array1, array2) {
+  let returnValue = false;
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] === array2[i]) {
+        returnValue = true;
+      } else {
+        returnValue = false;
+      }
+    }
+  }
+  return returnValue;
+};
+
+// Function to print appropriate message
+const assertArraysEqual = function(array1, array2) {
+  let comparisonResult = eqArrays(array1, array2);
+  if (comparisonResult === true) {
+    console.log("✅ Both Array's are Equal");
+  } else {
+    console.log("❌ Both Array's are not Equal");
+  }
+   
+};
+
+
+// TEST CODE
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
