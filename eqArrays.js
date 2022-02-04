fa00b5cb6f40d5db2f2f91d5ff04aa34b4eb7c03
Harshbir Singh <singh.hveer1@gmail.com>
2022-02-04 00:03:44 -0700

    Fixed eqArrays funtion to compare empty array's too

--- eqArrays.js
+++ eqArrays.js
@@ -9,11 +9,15 @@ const assertEqual = function(actual, expected) {
 const eqArrays = function(array1, array2) {
   let returnValue = false;
   if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] === array2[i]) {
-        returnValue = true;
-      } else {
-        returnValue = false;
+    if (array1.length === 0) {
+      returnValue = true;
+    } else {
+      for (let i = 0; i < array1.length; i++) {
+        if (array1[i] === array2[i]) {
+          returnValue = true;
+        } else {
+          returnValue = false;
+        }
       }
     }
   }
