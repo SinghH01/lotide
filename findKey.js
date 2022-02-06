40a5e22a962f80f0d034750749aa76c0c1134012
Harshbir Singh <singh.hveer1@gmail.com>
2022-02-06 15:20:33 -0700

    Created findKey function

---
+++ findKey.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  let result = undefined;
+  for (const item in object) {
+    if (callback(object[item]) === true) {
+      console.log(item);
+      result = item;
+      break;
+    }
+  }
+  return result;
+};
+
+
+
+
+// TEST CODE
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
