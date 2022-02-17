7b1744c7d014f30c90748b95ec8e7629b4e856cb
Harshbir Singh <singh.hveer1@gmail.com>
2022-02-17 00:43:25 -0700

    Refactor with Mocha and Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,14 +1,27 @@
 // Import functions
-const assertArraysEqual = require(`../assertArraysEqual`);
 const middle = require(`../middle`);
+const assert = require("chai").assert;
 
 
 // TEST CODE
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]),[]);
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-
-assertArraysEqual(middle([1, 2, 3, 4]), [2,3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]);
+describe("#middle", () => {
+  it('returns empty array [] if there is only one element in array', () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it('returns empty array [] if there are only two elements in an array', () => {
+    assert.deepEqual(middle([1, 2]),[]);
+  });
+  it('returns [2] for [1, 2, 3]', () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it('returns [3] for [1, 2, 3, 4, 5]', () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it('returns [2, 3] for [1, 2, 3, 4]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2,3]);
+  });
+  it('returns [3, 4] for [1, 2, 3, 4, 5, 6]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,23 +1,17 @@
-//Importing Functions
-const tail = require(`../tail`);
-const assertEqual = require(`../assertEqual`);
+// Import functions
+const assert = require('chai').assert;
+const tail   = require('../tail');
 
 
 //TEST CODE
-
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-
-//Test Case 2: Crray with only one element should yield an empty array for its tail
-const result2 = tail(["Hello"]);
-assertEqual(result2.length,0);
-assertEqual(result2[0], undefined);
-
-//Test Case3: Empty array should yield an empty array for its tail
-const result3 = tail([]);
-assertEqual(result3.length,0);
-assertEqual(result3[0], undefined);
+describe("#tail", () => {
+  it('returns ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+  it('returns empty array [] for an array with one element', () => {
+    assert.deepEqual(tail(["Hello"]),[]);
+  });
+  it('returns an empty array [] for emply array []', () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
