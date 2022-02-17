432a5da04ad0bba4dd712e1864f5cd8768985251
Harshbir Singh <singh.hveer1@gmail.com>
2022-02-16 22:44:46 -0700

    Export eqArray function amd create seperate test file

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// Compare two arrays to be equal or not
 
 const eqArrays = function(array1, array2) {
   let returnValue = false;
@@ -24,10 +18,4 @@ const eqArrays = function(array1, array2) {
   return returnValue;
 };
 
-
-// TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);// => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+module.exports = eqArrays;
