73f0cb6e778d1d91dcbf8ffc88227d962fc44679
Harshbir Singh <singh.hveer1@gmail.com>
2022-02-16 23:12:46 -0700

    Export middle function

--- middle.js
+++ middle.js
@@ -1,33 +1,4 @@
-//Function to compare two arrays
-const eqArrays = function(array1, array2) {
-  let returnValue = false;
-  if (array1.length === array2.length) {
-    if (array1.length === 0) {
-      returnValue = true;
-    } else {
-      for (let i = 0; i < array1.length; i++) {
-        if (array1[i] === array2[i]) {
-          returnValue = true;
-        } else {
-          returnValue = false;
-        }
-      }
-    }
-  }
-  return returnValue;
-};
-
-// Function to print appropriate message after comparing two array's
-const assertArraysEqual = function(array1, array2) {
-  let comparisonResult = eqArrays(array1, array2);
-  if (comparisonResult === true) {
-    console.log("✅ Both Array's are Equal");
-  } else {
-    console.log("❌ Both Array's are not Equal");
-  }
-   
-};
-
+// Function to find middle of an array
 const middle = function(array) {
   let returnArray = [];
 
@@ -47,12 +18,4 @@ const middle = function(array) {
   return returnArray;
 };
 
-// TEST CODE
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]),[]);
-
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-
-assertArraysEqual(middle([1, 2, 3, 4]), [2,3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]);
+module.exports = middle;
