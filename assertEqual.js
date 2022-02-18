e46a463f82b2186abcc5d67fc0213f56b406654e
Harshbir Singh <singh.hveer1@gmail.com>
2022-02-17 23:50:46 -0700

    Add return value based on comparison

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,11 @@
 // Compares two values to be equal or not
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+    // console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+    return true;
   } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+    //console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+    return false;
   }
 };
 
